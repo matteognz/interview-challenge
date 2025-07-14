f2b2e826f1aba0fe75163966d1b0fa87470c5c0a
Matteo <matteo.gonzi99@gmail.com>
2025-07-15 01:42:27 +0200

    docs(patient): patients api documentation
    TODO: Assign medications to patients -> A patient can have multiple medication assignments (OneToMany relation)

--- backend/src/modules/patient/controller/patient.controller.ts
+++ backend/src/modules/patient/controller/patient.controller.ts
@@ -10,26 +10,39 @@ import {
 } from '@nestjs/common';
 import { PatientService } from '../service/patient.service';
 import { CreatePatientDto } from '../dto/create-patient.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('patients')
 @Controller('patients')
 export class PatientController {
   constructor(private readonly patientService: PatientService) {}
 
+  @ApiOperation({ summary: 'Create new patient' })
+  @ApiResponse({ status: 201, description: 'New patient created successfully' })
   @Post()
   create(@Body() dto: CreatePatientDto) {
     return this.patientService.create(dto);
   }
 
+  @ApiOperation({ summary: 'Retrieve all patients' })
+  @ApiResponse({ status: 200, description: 'List of all patients' })
   @Get()
   findAll() {
     return this.patientService.findAll();
   }
 
+  @ApiOperation({ summary: 'Retrieve patient by id' })
+  @ApiResponse({ status: 200, description: 'Patient found' })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.patientService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update patient by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Patient updated successfully',
+  })
   @Put(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -38,6 +51,11 @@ export class PatientController {
     return this.patientService.update(id, dto);
   }
 
+  @ApiOperation({ summary: 'Delete patient by id' })
+  @ApiResponse({
+    status: 204,
+    description: 'Patient deleted successfully',
+  })
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.patientService.remove(id);
--- backend/src/modules/patient/dto/create-patient.dto.ts
+++ backend/src/modules/patient/dto/create-patient.dto.ts
@@ -1,10 +1,21 @@
 import { IsDateString, IsNotEmpty, IsString } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePatientDto {
+  @ApiProperty({
+    example: 'Matteo Gonzi',
+    description: 'Patient name',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    example: '1999-06-01',
+    description: 'Date of Birth',
+    type: String,
+    format: 'date',
+  })
   @IsDateString()
   @IsNotEmpty()
   dateOfBirth: string;
