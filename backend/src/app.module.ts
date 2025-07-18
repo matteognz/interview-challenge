095288e6322b27cb398c155320ccccbd7a84e82c
Matteo <matteo.gonzi99@gmail.com>
2025-07-18 02:39:14 +0200

    refactor(sample): Removed samples from backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SampleModule } from './sample/sample.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PatientModule } from './modules/patient/patient.module';
 import { MedicationModule } from './modules/medication/medication.module';
 import { AssignmentModule } from './modules/assignment/assignment.module';
@@ -16,12 +13,11 @@ import { AssignmentModule } from './modules/assignment/assignment.module';
       synchronize: true,
       logging: process.env.NODE_ENV !== 'production',
     }),
-    SampleModule,
     PatientModule,
     MedicationModule,
     AssignmentModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
